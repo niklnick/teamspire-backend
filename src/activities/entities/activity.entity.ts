8e22c358d503cc31ddd6ba8176b352c28c158f12
neumann.niklas <neumann.niklas98@gmail.com>
2024-03-13 21:54:43 +0100

    event-users resource

--- src/activities/entities/activity.entity.ts
+++ src/activities/entities/activity.entity.ts
@@ -1,5 +1,6 @@
+import { EventUser } from "src/events/entities/event-user.entity";
 import { User } from "src/users/entities/user.entity";
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Activity {
@@ -12,4 +13,7 @@ export class Activity {
     @ManyToOne(() => User, { nullable: false, onDelete: 'CASCADE' })
     @JoinColumn({ name: 'creator_id' })
     creator: User;
+
+    @OneToMany(() => EventUser, (eventUser: EventUser) => eventUser.votedActivity)
+    userVotes: EventUser[];
 }
--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventActivity } from './entities/event-activity.entity';
+import { EventUser } from './entities/event-user.entity';
 import { Event } from './entities/event.entity';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Event, EventActivity])],
+  imports: [TypeOrmModule.forFeature([Event, EventActivity, EventUser])],
   controllers: [EventsController],
   providers: [EventsService]
 })
