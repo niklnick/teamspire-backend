57e72de9093d97a1739453247f2d6f8157ee1eb7
neumann.niklas <neumann.niklas98@gmail.com>
2024-03-13 12:29:32 +0100

    users ressource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppRoutingModlue } from './app-routing.module';
 import { TypeOrmConfigService } from './config/typeorm-config.service';
 
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true }),
-        TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService })
+        TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+        AppRoutingModlue
     ]
 })
 export class AppModule { }
